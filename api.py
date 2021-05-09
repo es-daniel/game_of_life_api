ea17ec29680edcb9e70a5215138ca6021a863e9d
DanielES <danielelizondo12@gmail.com>
2021-05-09 17:47:09 -0600

    Deploying to heroku

--- api.py
+++ api.py
@@ -1,50 +1,12 @@
-import flask
+from flask import Flask
 from flask_cors import CORS
-from utils import Utils
-import json
-from flask import request, jsonify
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
-utils = Utils()
+@app.route("/")
+def welcome():
+    return (f"Welcome to the RECIPERFECT search API!<br/>")
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<b>Game of life API</b>"
-
-@app.route('/start', methods=['GET'])
-def start():
-    pattern = request.args.get('pattern')
-    grid = []
-    print(pattern)
-    if pattern == 'rand':
-        grid = utils.build_new_grid(20, 25)  # 20 rows with 25 columns
-        utils.seed(grid)
-    elif pattern == 'gosper_glider_gun':
-        grid = utils.gosper_glider_gun()
-    elif pattern == 'pulsar':
-        grid = utils.pulsar()
-    save_grid(grid)
-    return jsonify(grid)
-
-
-@app.route('/update', methods=['GET'])
-def update():
-    current_grid = read_grid()
-    new_grid = utils.update_grid(current_grid)
-    save_grid(new_grid)
-    return jsonify(new_grid)
-
-
-def save_grid(grid):
-    db = open('history.json', 'w')
-    db.write(json.dumps(grid))
-    db.close()
-
-def read_grid():
-    grid = open('history.json')
-    return json.load(grid)
-
-
-app.run()
+if __name__ == '__main__':
+    app.run()
